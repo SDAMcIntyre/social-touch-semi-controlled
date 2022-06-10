005413cf8b3493fce4c385504a7e0ccfb4813688
Basil <basil.duvernoy@gmail.com>
2022-06-10 17:06:25 +0200

    try to add high priority

--- Expt_social-touch-neural.py
+++ Expt_social-touch-neural.py
@@ -1,12 +1,34 @@
 import os
-from pynput import keyboard
+
+import win32api
+import win32con
+import win32process
 from psychopy import core, data, gui
+from pynput import keyboard
 
 from modules.arduino_comm import ArduinoComm
-from modules.kinect_comm import KinectComm
-from modules.file_management import FileManager
 from modules.expert_interface import ExpertInterface
-from modules.audio_management import AudioManager
+from modules.file_management import FileManager
+from modules.kinect_comm import KinectComm
+
+
+def setpriority(pid=None, priority=5):
+    """ Set The Priority of a Windows Process.  Priority is a value between 0-5 where
+        2 is normal priority.  Default sets the priority of the current
+        python process but can take any valid process ID. """
+
+    priorityclasses = [win32process.IDLE_PRIORITY_CLASS,
+                       win32process.BELOW_NORMAL_PRIORITY_CLASS,
+                       win32process.NORMAL_PRIORITY_CLASS,
+                       win32process.ABOVE_NORMAL_PRIORITY_CLASS,
+                       win32process.HIGH_PRIORITY_CLASS,
+                       win32process.REALTIME_PRIORITY_CLASS]
+    if pid is None:
+        pid = win32api.GetCurrentProcessId()
+    handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
+    win32process.SetPriorityClass(handle, priorityclasses[priority])
+
+setpriority()
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_path)
