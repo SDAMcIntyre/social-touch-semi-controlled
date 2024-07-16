c4f9bbeed6868a124505a8b7eb139bad1467303a
Basil <basil.duvernoy@gmail.com>
2024-07-16 16:59:07 +0200

    added a save function to merge the 3 figures together

--- source/libraries/plot/semicontrolled_data_visualizer.py
+++ source/libraries/plot/semicontrolled_data_visualizer.py
@@ -1,40 +1,115 @@
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
 import numpy as np
+from PIL import Image
+import pyglet
 import seaborn as sns
 import tkinter as tk
 from tkinter import ttk
+import os
 
 from libraries.materials.semicontrolled_data import SemiControlledData  # noqa: E402
 
 
-
 class SemiControlledDataVisualizer:
-    def __init__(self, scd=None, plotVel=False, auto_positioning=False):
+    def __init__(self, scd=None, auto_positioning=False):
         # create the figures
-        self.fig2D_neur = DataVisualizer2D(2, "Neuron", auto_positioning=auto_positioning)
-
-        self.fig2D_contact = DataVisualizer2D(3, "Depth and Area", auto_positioning=auto_positioning)
+        self.fig2D_TTL = DataVisualizer2D(4, "TTLs", auto_positioning=auto_positioning)
+        self.fig2D_global = DataVisualizer2D(5, "Neuron, Depth, and Area", auto_positioning=auto_positioning)
+        
         self.figpos = DataVisualizer3D("Position", auto_positioning=auto_positioning)
-        if plotVel:
-            self.figvel = DataVisualizer3D("Velocity")
 
-        # (mandatory for the update function) boolean to know if the velocity has to be displayed
-        self.plotVel = plotVel
+        self.window_positioning()
 
         if isinstance(scd, SemiControlledData):
             self.update(scd)
 
+    def __del__(self):
+        del self.fig2D_TTL, self.fig2D_global, self.figpos
+
+    def window_positioning(self):
+        # Function to get screen size
+        def get_screen_size():
+            display = pyglet.canvas.Display()
+            screen = display.get_default_screen()
+            return screen.width, screen.height
+
+        screen_width, screen_height = get_screen_size()
+
+        # Set the positions and sizes of the figures
+        fig1_width, fig1_height = screen_width // 2, screen_height
+        fig2_width, fig2_height = screen_width // 2, screen_height * 3 // 5
+        fig3_width, fig3_height = screen_width // 2, screen_height * 2 // 5
+
+        # Helper function to move and resize the figures
+        def move_resize_figure(fig, x, y, width, height):
+            backend = plt.get_backend()
+            if backend == 'TkAgg':
+                fig.canvas.manager.window.wm_geometry(f"{width}x{height}+{x}+{y}")
+            elif backend == 'Qt5Agg':
+                fig.canvas.manager.window.setGeometry(x, y, width, height)
+            elif backend == 'WXAgg':
+                fig.canvas.manager.window.SetPosition((x, y))
+                fig.canvas.manager.window.SetSize((width, height))
+            else:
+                print(f"Backend {backend} is not supported")
+
+        # Position the figures
+        move_resize_figure(self.figpos.fig, 0, 0, fig1_width, fig1_height)
+        move_resize_figure(self.fig2D_global.fig, fig1_width, 0, fig2_width, fig2_height)
+        move_resize_figure(self.fig2D_TTL.fig, fig1_width, fig2_height, fig3_width, fig3_height)
+
+    def save(self, output_filename_abs):
+        # set correct dimensions
+        display = pyglet.canvas.Display()
+        screen = display.get_default_screen()
+        screenratio = screen.width / screen.height
+        dpi = 100
+        height = 1080 / dpi
+        width = screenratio * height
+        self.figpos.fig.set_size_inches(width / 2, height)
+        self.fig2D_global.fig.set_size_inches(width / 2, height * 2 / 3)
+        self.fig2D_TTL.fig.set_size_inches(width / 2, height * 1 / 3)
+
+        # temporarily save the figures
+        self.figpos.fig.savefig("fispos_tmp.png")
+        self.fig2D_global.fig.savefig("global_tmp.png")
+        self.fig2D_TTL.fig.savefig("ttl_tmp.png")
+
+        # Load the temporary images
+        figpos = Image.open("fispos_tmp.png")
+        fig2D_global = Image.open("global_tmp.png")
+        fig2D_TTL = Image.open("ttl_tmp.png")
+
+        # Get the image dimensions
+        width1, height1 = figpos.size
+        width2, height2 = fig2D_global.size
+        width3, height3 = fig2D_TTL.size
+
+        # Create a new image with the combined height of the three images
+        combined_image = Image.new('RGB', (width1 + width2, height1))
+
+        # Get the dimensions of each image
+        combined_image.paste(figpos, (0, 0))
+        combined_image.paste(fig2D_global, (width1, 0))
+        combined_image.paste(fig2D_TTL, (width1, height2))
+
+        # Save the combined image
+        combined_image.save(output_filename_abs)
+
+        os.remove("fispos_tmp.png")
+        os.remove("global_tmp.png")
+        os.remove("ttl_tmp.png")
+
+
     def set_lim(self, fig_choice, limits):
         match fig_choice:
             case "Depth and Area":
-                self.fig2D_contact.set_lim(limits)
+                self.fig2D_global.set_lim(limits)
             case "Position":
                 self.figpos.set_lim(limits)
-            case "Velocity":
-                if self.plotVel:
-                    self.figvel.set_lim(limits)
 
     def update(self, scd: SemiControlledData, title=None):
         time = scd.md.time
@@ -47,19 +122,29 @@ class SemiControlledDataVisualizer:
                     f"Size: {scd.stim.size}\n"
                     f"Velocity: {scd.stim.vel} cm/s")
 
-        self.fig2D_neur.update(0, time, scd.neural.spike, 'Spike')
-        self.fig2D_neur.update(1, time, scd.neural.iff, 'iff')
+        self.fig2D_TTL.update(0, time, scd.neural.TTL, 'TTL Nerve')
+        self.fig2D_TTL.update(1, time, scd.contact.TTL, 'TTL contact')
+        self.fig2D_TTL.update(2, time, scd.neural.TTL, 'both')
+        self.fig2D_TTL.update(2, time, scd.contact.TTL, 'both', linestyle="--", reset=False)
+        self.fig2D_TTL.update(3, time, scd.neural.TTL-scd.contact.TTL, 'diff')
 
-        self.fig2D_contact.update(0, time, scd.contact.pos_1D, 'Position (Principal Component)')
-        self.fig2D_contact.update(1, time, scd.contact.depth, 'Depth')
-        self.fig2D_contact.update(2, time, scd.contact.area, 'Area size')
+        self.fig2D_global.update(0, time, scd.contact.pos_1D, 'Position (Principal Component)', showxlabel=False)
+        self.fig2D_global.update(1, time, scd.contact.depth, 'Depth', showxlabel=False)
+        self.fig2D_global.update(2, time, scd.contact.area, 'Area size', showxlabel=False)
+        self.fig2D_global.update(3, time, scd.neural.iff, 'IFF', showxlabel=False)
+        self.fig2D_global.update(4, time, scd.neural.spike, 'Spikes', showxlabel=True)
 
         self.figpos.update(time, scd.contact.pos, info_str)
         if title is not None:
             self.figpos.fig.suptitle(title)
 
-        if self.plotVel:
-            self.figvel.update(time, scd.contact.vel, info_str)
+    def add_vertical_lines(self, xlocs):
+        self.fig2D_global.add_vertical_lines(xlocs)
+
+        for ax in self.fig2D_global.axs:
+            # Add red vertical lines at specified x-locations
+            for xloc in xlocs:
+                ax.axvline(x=xloc, color='red', linestyle='--', linewidth=1)
 
 
 class DataVisualizer2D:
@@ -90,12 +175,19 @@ class DataVisualizer2D:
     def set_lim(self, limits):
         self.limits = limits
 
-    def update(self, ax_idx, time: list[float], data: list[float], data_name):
-        self.axs[ax_idx].clear()
+    def update(self, ax_idx, time: list[float], data: list[float], data_name, linestyle="-", reset=True, showxlabel=True):
+        if reset:
+            self.axs[ax_idx].clear()
 
-        self.axs[ax_idx].plot(time, data, label=data_name)
+        # Remove NaNs
+        valid = ~(np.isnan(data))
+        data = data[valid]
+        time = time[valid]  # ensure time array is also filtered
+
+        self.axs[ax_idx].plot(time, data, label=data_name, linestyle=linestyle)
         self.axs[ax_idx].set_title(data_name)
-        self.axs[ax_idx].set_xlabel('Time')
+        if showxlabel:
+            self.axs[ax_idx].set_xlabel('Time')
         self.axs[ax_idx].set_ylabel(data_name)
         self.axs[ax_idx].legend()
 
@@ -108,6 +200,9 @@ class DataVisualizer2D:
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
+    def add_vertical_lines(self, xlocs):
+        pass
+
 
 class DataVisualizer3D:
     def __init__(self, title, auto_positioning=False):
@@ -115,9 +210,6 @@ class DataVisualizer3D:
         self.fig = plt.figure(figsize=(8, 6))
         self.ax = self.fig.add_subplot(111, projection='3d')
         plt.ion()
-
-        #if pos is not None:
-        #    self.fig.canvas.manager.window.setGeometry(pos[0], pos[1], pos[2], pos[3])
 
         self.fig.show()
 
@@ -131,15 +223,34 @@ class DataVisualizer3D:
     def set_lim(self, limits):
         self.limits = limits
 
-    def update(self, time, data: list[float], info_str=None, colorsMap='viridis_r'):
+    def update(self, time, data: list[float], info_str=None, colorsMap='viridis_r', withLine=True):
         if self.fig.texts:
             for text in self.fig.texts:
                 text.remove()
         self.ax.clear()
 
-        # Scatter plot with color gradient based on time
         x, y, z = data
+
+        # Remove NaNs
+        valid = ~(np.isnan(x) | np.isnan(y) | np.isnan(z))
+        x = x[valid]
+        y = y[valid]
+        z = z[valid]
+        time = time[valid]  # ensure time array is also filtered
+
+        # Scatter plot with color gradient based on time
         sc = self.ax.scatter(x, y, z, c=time, cmap=colorsMap, s=self.dot_size)
+
+        if withLine:
+            # Create line segments
+            points = np.array([x, y, z]).T.reshape(-1, 1, 3)
+            segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+            # Normalize time for color mapping
+            norm = plt.Normalize(time.min(), time.max())
+            lc = Line3DCollection(segments, cmap=colorsMap, norm=norm)
+            lc.set_array(time)
+            self.ax.add_collection(lc)
 
         # Add a color bar
         if self.cbar is None:
