d801dc78d5f43cc8925e4b5f1d0ade745925ef8c
Basil <basil.duvernoy@gmail.com>
2024-08-05 11:56:08 +0200

    Script to generate a visual report of the data for each SCdata object

--- source/6_analysis/6.1_analysis_SemiControlledData.py
+++ source/6_analysis/6.1_analysis_SemiControlledData.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+from PIL import Image
 import re
 import sys
 import warnings
@@ -10,26 +11,29 @@ import time
 
 # homemade libraries
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from libraries.preprocessing.semicontrolled_data_manager import SemiControlledDataManager  # noqa: E402
+from libraries.materials.semicontrolled_data import SemiControlledData  # noqa: E402
+from libraries.processing.semicontrolled_data_manager import SemiControlledDataManager  # noqa: E402
+from libraries.processing.semicontrolled_data_splitter import SemiControlledDataSplitter  # noqa: E402
+from libraries.plot.semicontrolled_data_visualizer import SemiControlledDataVisualizer  # noqa: E402
 import libraries.misc.path_tools as path_tools  # noqa: E402
 from libraries.misc.waitforbuttonpress_popup import WaitForButtonPressPopup
-from libraries.plot.semicontrolled_data_visualizer import SemiControlledDataVisualizer  # noqa: E402
 import libraries.plot.semicontrolled_data_visualizer_unitcontact as scdata_visualizer_neur  # noqa: E402
 
 
 if __name__ == "__main__":
-    force_processing = True  # If user wants to force data processing even if results already exist
-    show = False  # If user wants to monitor what's happening
+    force_processing = False  # If user wants to force data processing even if results already exist
+    show = True  # If user wants to monitor what's happening
+    save_figures = True
     save_results = True
 
     print("Step 0: Extract the videos embedded in the selected sessions.")
     # get database directory
     db_path = os.path.join(path_tools.get_database_path(), "semi-controlled")
     # get input data directory
-    db_path_input = os.path.join(db_path, "merged", "kinect_and_nerve", "1_by-trials")
+    db_path_input = os.path.join(db_path, "3_merged", "2_kinect_and_nerve", "1_by-trials")
     # get metadata paths
-    md_stimuli_path = os.path.join(db_path, "primary", "logs", "stimuli_by_blocks")
-    md_neuron_filename_abs = os.path.join(db_path, "primary", "nerve", "semicontrol_unit-name_to_unit-type.csv")
+    md_stimuli_path = os.path.join(db_path, "1_primary", "logs", "stimuli_by_blocks")
+    md_neuron_filename_abs = os.path.join(db_path, "1_primary", "nerve", "semicontrol_unit-name_to_unit-type.csv")
     # get output directory
     result_path = os.path.join(path_tools.get_result_path(), "semi-controlled")
     db_path_output = os.path.join(result_path, "kinect_and_nerve", "0_by-trials")
@@ -60,12 +64,14 @@ if __name__ == "__main__":
                      '2022-06-22_ST18-04']
     sessions = []
     sessions = sessions + sessions_ST13
-    #sessions = sessions + sessions_ST14
-    #sessions = sessions + sessions_ST15
-    #sessions = sessions + sessions_ST16
-    #sessions = sessions + sessions_ST18
+    sessions = sessions + sessions_ST14
+    sessions = sessions + sessions_ST15
+    sessions = sessions + sessions_ST16
+    sessions = sessions + sessions_ST18
     print(sessions)
 
+    if show:
+        scd_visualiser = SemiControlledDataVisualizer()
     scd_list = []
     # it is important to split by MNG files / neuron recordings to create the correct subfolders.
     for session in sessions:
@@ -88,19 +94,79 @@ if __name__ == "__main__":
                 continue
 
             # 3. create a SemiControlledData's list of TOUCH EVENT:
-            scdm = SemiControlledDataManager()
-            data_sliced = scdm.preprocess_data_file(data_filename_abs, md_stimuli_filename_abs, md_neuron_filename_abs,
-                                                    correction=True, show=False, verbose=True)
+            # 3.1 load the data
+            scd = SemiControlledData(data_filename_abs, md_stimuli_filename_abs, md_neuron_filename_abs)  # resources
+            scd.set_variables(dropna=False)
+            if show:
+                scd_visualiser.update(scd)
+                if save_figures:
+                    path_fig = os.path.join(db_path_output, "trials_overview")
+                    if not os.path.exists(path_fig):
+                        os.makedirs(path_fig)
+                    output_filename = os.path.join(path_fig, data_filename.replace(".csv", ".png"))
+                    if not force_processing:
+                        try:
+                            with open(output_filename, 'r'):
+                                print("Result file exists, jump to the next dataset.")
+                                continue
+                        except FileNotFoundError:
+                            pass
 
-            # store the calculated data
-            scd_list.append(data_sliced)
+                    # set correct dimensions
+                    screenratio = 1920/1080
+                    dpi = 100
+                    height = 1080 / dpi
+                    width = screenratio * height
+                    scd_visualiser.figpos.fig.set_size_inches(width/2, height)
+                    scd_visualiser.fig2D_global.fig.set_size_inches(width/2, height*2/3)
+                    scd_visualiser.fig2D_TTL.fig.set_size_inches(width/2, height*1/3)
+
+                    # temporarily save the figures
+                    scd_visualiser.figpos.fig.savefig(os.path.join(db_path_output, "fispos_tmp.png"))
+                    scd_visualiser.fig2D_global.fig.savefig(os.path.join(db_path_output, "global_tmp.png"))
+                    scd_visualiser.fig2D_TTL.fig.savefig(os.path.join(db_path_output, "ttl_tmp.png"))
+
+                    # Load the temporary images
+                    figpos = Image.open(os.path.join(db_path_output, "fispos_tmp.png"))
+                    fig2D_global = Image.open(os.path.join(db_path_output, "global_tmp.png"))
+                    fig2D_TTL = Image.open(os.path.join(db_path_output, "ttl_tmp.png"))
+
+                    # Get the image dimensions
+                    width1, height1 = figpos.size
+                    width2, height2 = fig2D_global.size
+                    width3, height3 = fig2D_TTL.size
+
+                    # Create a new image with the combined height of the three images
+                    combined_image = Image.new('RGB', (width1+width2, height1))
+
+                    # Get the dimensions of each image
+                    combined_image.paste(figpos, (0, 0))
+                    combined_image.paste(fig2D_global, (width1, 0))
+                    combined_image.paste(fig2D_TTL, (width1, height2))
+
+                    # Save the combined image
+                    combined_image.save(output_filename)
+
+                    # Optional: Show the combined image
+                    #combined_image.show()
+                else:
+                    WaitForButtonPressPopup()
 
 
-    scd_visualiser = SemiControlledDataVisualizer()
+            # 3.2 split by touch event
+            if 0:
+                splitter = SemiControlledDataSplitter()  # tools
+                data_sliced = splitter.split_by_touch_event(scd, correction=True, show=True)
+
+                # 4. store the calculated data
+                scd_list.append(data_sliced)
+
     flattened_scd_list = list(itertools.chain.from_iterable(scd_list))
-    for scd in flattened_scd_list:
-        scd_visualiser.update(scd)
-        WaitForButtonPressPopup()
+
+    if show:
+        for scd in flattened_scd_list:
+            scd_visualiser.update(scd)
+            WaitForButtonPressPopup()
 
     print("done.")
 
