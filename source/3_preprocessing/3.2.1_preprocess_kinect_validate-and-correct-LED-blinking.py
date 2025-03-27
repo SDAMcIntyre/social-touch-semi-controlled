82386ebea9cb7ddcd5d43571d798cfd1caf4ed5d
Basil <basil.duvernoy@gmail.com>
2025-03-27 10:15:32 +0100

    minor modification in the merging LED - contact data  and the correction of LED ON/OFF

--- source/3_preprocessing/3.2.1_preprocess_kinect_validate-and-correct-LED-blinking.py
+++ source/3_preprocessing/3.2.1_preprocess_kinect_validate-and-correct-LED-blinking.py
@@ -12,6 +12,22 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import libraries.misc.path_tools as path_tools  # noqa: E402
 
 
+def create_log_filename(input_string):
+    # Extract date
+    date_pattern = r'\d{4}-\d{2}-\d{2}'
+    date_match = re.search(date_pattern, input_string)
+    date = date_match.group() if date_match else None
+
+    # Extract integers from the input string without date
+    integer_pattern = r'\d+'
+    integers = re.findall(integer_pattern, input_string.replace(date, ""))
+
+    # Construct new string with the extracted date and integers
+    output_string = f"{date}_ST{integers[0]}-{integers[1]}_semicontrolled_block-order{integers[2]}_stimuli.csv"
+
+    return output_string
+
+
 def find_groups_of_ones(arr):
     groups = []
     in_group = False
@@ -34,31 +50,6 @@ def find_groups_of_ones(arr):
     return groups
 
 
-def group_lengths2(arr):
-    if not arr:
-        return []
-
-    group_length = []
-    group_label = []
-    current_length = 1
-    current_value = arr[0]
-
-    for i in range(1, len(arr)):
-        if arr[i] == current_value:
-            current_length += 1
-        else:
-            # save previous group length and label/value
-            group_length.append(current_length)
-            group_label.append(current_value)
-            current_value = arr[i]
-            current_length = 1
-
-    # save the last group length and label/value
-    group_length.append(current_length)
-    group_label.append(current_value)
-
-    return group_length, group_label
-
 
 def group_lengths(arr):
     if arr.size == 0:
@@ -75,6 +66,7 @@ def group_lengths(arr):
     labels = arr[indices[:-1]]
 
     return lengths, labels
+
 
 
 # merge neighbouring values to the small number
@@ -115,111 +107,105 @@ def merge_small_group_with_neighbour(values, labels, val_max=10):
 
 
 if __name__ == "__main__":
-    force_processing = True  # If user wants to force data processing even if results already exist
-    show = False  # If user wants to monitor what's happening
+    OS_linux = False
 
-    save_results = False
+    force_processing = True  # If user wants to force data processing even if results already exist
+    save_results = True
     generate_report = False
 
-    print("Step 0: Extract the videos embedded in the selected neurons.")
-    # get metadata dataframe
-    md_path = os.path.join(path_tools.get_database_path(), "semi-controlled", "1_primary", "logs", "2_stimuli_by_blocks")
+    input_filename_ending = '_kinect_LED.csv'
+    # usually, blinking is 100ms maximum, which is equivalent to 4 frames at 30 Hz (Kinect Fs).
+    blinking_id_nframe_max = 4
+    show_warning = False  # If user wants to monitor what's happening
+    show = False  # If user wants to monitor what's happening
 
+    print("Step 0: Extract the videos embedded in the selected sessions.")
     # get database directory
-    db_path = os.path.join(path_tools.get_database_path(), "semi-controlled", "2_processed", "kinect", "led")
+    db_path_linux = os.path.expanduser('~/Documents/datasets/semi-controlled')
+    if OS_linux:
+        db_path = db_path_linux
+    else:
+        db_path = os.path.join(path_tools.get_database_path(), "semi-controlled")
     # get input base directory
-    db_path_input = os.path.join(db_path, "0_block-order")
-    # get output base directory
-    db_path_output = os.path.join(db_path, "1_block-order_corrected")
-
-    if save_results and not os.path.exists(db_path_output):
+    db_path_md = os.path.join(db_path, "1_primary", "logs", "2_stimuli_by_blocks")
+    db_path_input = os.path.join(db_path, "2_processed", "kinect")
+    # set output base directory
+    db_path_output = os.path.join(db_path, "2_processed", "kinect")
+    if not os.path.exists(db_path_output):
         os.makedirs(db_path_output)
         print(f"Directory '{db_path_output}' created.")
 
     # neuron names
-    neurons_ST13 = ['2022-06-14_ST13-01',
+    sessions_ST13 = ['2022-06-14_ST13-01',
                     '2022-06-14_ST13-02',
                     '2022-06-14_ST13-03']
 
-    neurons_ST14 = ['2022-06-15_ST14-01',
+    sessions_ST14 = ['2022-06-15_ST14-01',
                     '2022-06-15_ST14-02',
                     '2022-06-15_ST14-03',
                     '2022-06-15_ST14-04']
 
-    neurons_ST15 = ['2022-06-16_ST15-01',
+    sessions_ST15 = ['2022-06-16_ST15-01',
                     '2022-06-16_ST15-02']
 
-    neurons_ST16 = ['2022-06-17_ST16-02',
+    sessions_ST16 = ['2022-06-17_ST16-02',
                     '2022-06-17_ST16-03',
                     '2022-06-17_ST16-04',
                     '2022-06-17_ST16-05']
 
-    neurons_ST18 = ['2022-06-22_ST18-01',
+    sessions_ST18 = ['2022-06-22_ST18-01',
                     '2022-06-22_ST18-02',
                     '2022-06-22_ST18-04']
-    neurons = []
-    neurons = neurons + neurons_ST13
-    neurons = neurons + neurons_ST14
-    neurons = neurons + neurons_ST15
-    neurons = neurons + neurons_ST16
-    neurons = neurons + neurons_ST18
-    print(neurons)
+    sessions = []
+    sessions = sessions + sessions_ST13
+    sessions = sessions + sessions_ST14
+    sessions = sessions + sessions_ST15
+    sessions = sessions + sessions_ST16
+    sessions = sessions + sessions_ST18
+    print(sessions)
 
     diff_ms_all = []
     names_contact = []
     names_led = []
     # it is important to split by MNG files / neuron recordings to create the correct subfolders.
-    for neuron in neurons:
-        curr_led_data = os.path.join(db_path_input, neuron)
-
-        files_led_abs, files_led = path_tools.find_files_in_directory(curr_led_data, ending='_LED.csv')
+    for session in sessions:
+        # files_abs, files, _ = find_metadata_files(db_path_input, session)
+        curr_dir = os.path.join(db_path_input, session)
+        files_abs, files = path_tools.find_files_in_directory(curr_dir, ending=input_filename_ending)
         
-        for file_led_abs, file_led in zip(files_led_abs, files_led):
-            print(f"current file: {file_led}")
-            # 1. check if results already exists
-            output_filename = file_led  # file_led.replace(".csv", "_corrected.csv")
-            output_dir_abs = os.path.join(db_path_output, neuron)
-            output_filename_abs = os.path.join(output_dir_abs, output_filename)
-            if not force_processing:
-                try:
-                    with open(output_filename_abs, 'r'):
-                        print("Result file exists, jump to the next dataset.")
-                        continue
-                except FileNotFoundError:
-                    pass
+        for file_abs, file in zip(files_abs, files):
+            print(f"current file: {file}")
+            output_dirname = os.path.dirname(file_abs).replace(db_path_input, db_path_output)
+            output_filename = file.replace(".csv", "_corrected.csv")
+            output_filename_abs = os.path.join(output_dirname, output_filename)
 
+            stimuli_info_filename = create_log_filename(file)
+            stimuli_info_filename_abs = os.path.join(db_path_md, session, stimuli_info_filename)
+            
             # 2. check if led and nerve files exist for this contact file
-            stimuli_info_file = os.path.join(md_path, neuron, file_led.replace("_LED.csv", "_stimuli.csv"))
-            try:
-                with open(stimuli_info_file, 'r'):
-                    pass
-            except FileNotFoundError:
+            if not force_processing and os.path.exists(output_filename_abs):
+                print(f"File '{file}' already exists and the processing is not forced. Move to next file...")
+                continue
+            if not os.path.exists(stimuli_info_filename_abs):
                 print("Matching stimuli file does not exist.")
                 continue
-
-            # 3. Load meaningful data
-            # 3.1 from stimulus info
-            stimuli = pd.read_csv(stimuli_info_file)
-            # numbers of initial expected blinking
-            run_block_id = stimuli["run_block_id"].values[0]
-            # usually, blinking is 100ms maximum, which is equivalent to 4 frames at 30 Hz (Kinect Fs).
-            blinking_id_nframe_max = 4
-            # number of trials
-            n_trials = len(stimuli["run_block_id"])
+            
+            # extract block id and number of trials
+            stimuli = pd.read_csv(stimuli_info_filename_abs)
+            block_id = stimuli["run_block_id"].values[0]
+            ntrials = len(stimuli)
 
             # 3.2 from LED info
-            led = pd.read_csv(file_led_abs)
+            led = pd.read_csv(file_abs)
             led_on = led["LED on"].values
 
             # 4. work
-
             # use Nerve_TTL as only the good nerve signal have been kept
             on_signal_idx = find_groups_of_ones(led_on)
 
             # extract the identifier indexes of the TTL (1, 2, 3, or 4)
             on_signal_lengths = [len(sublist) for sublist in on_signal_idx]
             is_potential_block_id = [length < blinking_id_nframe_max for length in on_signal_lengths]
-
             # 4.2 count the number of True in a row at the start of the block
             led_block_id = 0
             for value in is_potential_block_id:
@@ -227,64 +213,55 @@ if __name__ == "__main__":
                     led_block_id += 1
                 else:
                     break
-            if led_block_id != run_block_id:
+            if led_block_id != block_id:
                 w = (f"Issue detected: Value of the block id tracked in the FRAMES is not what is expected:\n"
-                     f"(Expected){run_block_id} vs {led_block_id}(Tracked)")
+                     f"(Expected){block_id} vs {led_block_id}(Tracked)")
                 warnings.warn(w)
-                plt.plot(led_on)
-                plt.title(f"True block id = {run_block_id}")
-                plt.show()
+                if show_warning: 
+                    plt.plot(led_on)
+                    plt.title(f"{file}\nError in Block ID blinking\nExpected vs LED block id = {block_id} vs {led_block_id}")
+                    plt.show(block=True)
                 continue
-                plt.close()
 
             # 4.2 count the number of trials
-            without_block_id = on_signal_idx[led_block_id-1][-1] + 1
-            led_on_trials = led_on[without_block_id:]
+            block_id_end_loc = on_signal_idx[led_block_id-1][-1]
+            led_on_trials_only = led_on[(block_id_end_loc+1):]
             # Replace NaN values with zero
-            nan_mask = np.isnan(led_on_trials)
-            led_on_trials[nan_mask] = 0
+            nan_mask = np.isnan(led_on_trials_only)
+            led_on_trials_only[nan_mask] = 0
             # get length of continuous labels (0 or 1)
-            groups_length, groups_label = group_lengths(led_on_trials)
+            groups_length, groups_label = group_lengths(led_on_trials_only)
             # remove potential small undetected LED on to have consistent trial groups
             groups_length, groups_label = merge_small_group_with_neighbour(groups_length, groups_label, val_max=10)
             # sum all groups that are 1 (meaning it's a trial)
-            led_n_trials = sum(groups_label)
-            if led_n_trials != n_trials:
+            led_ntrials = sum(groups_label)
+            if led_ntrials != ntrials:
                 w = (f"Issue detected: Value of the number of trials in the FRAMES is not what is expected:\n"
-                     f"(Expected){n_trials} vs {led_n_trials}(Tracked)")
+                     f"(Expected){ntrials} vs {led_ntrials}(Tracked)")
                 warnings.warn(w)
-                plt.plot(led_on_trials)
-                plt.title(f"True of trials = {n_trials}")
-                plt.show()
+                if show_warning: 
+                    plt.plot(led_on_trials_only)
+                    plt.title(f"{file}\nError in the number of trials\nExpected vs LED trials number = {ntrials} vs {led_ntrials}")
+                    plt.show(block=True)
                 continue
-                plt.close()
 
-            # 5. create output data
-            df_output = []
-
-            # keep variables for the report
-            if generate_report:
-                pass
-
+            # 5. create output data with corrected block trials and initial block id blinking
+            arr_output = np.concatenate([led_on[:block_id_end_loc+1], np.repeat(groups_label, groups_length)])
+            
             if show:
-                pass
+                fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
+                ax1.plot(led_on)
+                ax1.set_title('Input')
+                ax2.plot(arr_output)
+                ax2.set_title('Output')
+                plt.show(block=True)
 
             # save data on the hard drive ?
             if save_results:
-                if not os.path.exists(output_dir_abs):
-                    os.makedirs(output_dir_abs)
-                df_output.to_csv(output_filename_abs, index=False)
-
-    if generate_report:
-        report_filename = os.path.join(db_path_output, "frame_differences_report.csv")
-        report_data = []
-        for name_contact, name_led, diff_ms in zip(names_contact, names_led, diff_ms_all):
-            report_data.append({"filename_contact": name_contact, "filename_led": name_led, "frame_difference": diff_ms})
-        with open(report_filename, mode='w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=["filename_contact", "filename_led", "frame_difference"])
-            writer.writeheader()
-            for row in report_data:
-                writer.writerow(row)
+                if not os.path.exists(output_dirname):
+                    os.makedirs(output_dirname)
+                led["LED on"] = arr_output
+                led.to_csv(output_filename_abs, index=False)
 
     print("done.")
 
--- source/3_preprocessing/3.2.3_preprocess_contact_incorporate-LED-as-TTL.py
+++ source/3_preprocessing/3.2.3_preprocess_contact_incorporate-LED-as-TTL.py
@@ -11,23 +11,54 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import libraries.misc.path_tools as path_tools  # noqa: E402
 
 
-if __name__ == "__main__":
+
+def create_output_filename(contact_file, led_file):
+    date_pattern = r'\d{4}-\d{2}-\d{2}'
+    input_string = contact_file
+
+    # Extract date
+    date_match = re.search(date_pattern, input_string)
+    date = date_match.group() if date_match else None
+    if date is None:
+        input_string = led_file
+        date_match = re.search(date_pattern, input_string)
+        date = date_match.group() if date_match else None
+
+    # Extract integers from the input string without date
+    integer_pattern = r'\d+'
+    integers = re.findall(integer_pattern, input_string.replace(date, ""))
+
+    # Construct new string with the extracted date and integers
+    output_string = f"{date}_ST{integers[0]}-{integers[1]}_semicontrolled_block-order{integers[2]}_kinect.csv"
+
+    return output_string
+
+
+
+if __name__ == "__main__":   
+    OS_linux = False
+
     force_processing = True  # If user wants to force data processing even if results already exist
     save_results = True
     generate_report = True
 
+    input_contact_filename_ending = 'somatosensory_data.csv'
+    input_led_filename_ending = '_LED.csv'
+    show_warning = False  # If user wants to monitor what's happening
     show = False  # If user wants to monitor what's happening
 
     print("Step 0: Extract the videos embedded in the selected sessions.")
     # get database directory
-    db_path = os.path.join(path_tools.get_database_path(), "semi-controlled", "2_processed", "kinect")
-
+    db_path_linux = os.path.expanduser('~/Documents/datasets/semi-controlled')
+    if OS_linux:
+        db_path = db_path_linux
+    else:
+        db_path = os.path.join(path_tools.get_database_path(), "semi-controlled")
     # get input base directory
-    db_path_input_contact = os.path.join(db_path, "contact", "1_block-order")
-    db_path_input_led = os.path.join(db_path, "led", "0_block-order")
-
-    # get output base directory
-    db_path_output = os.path.join(db_path, "contact_and_led", "0_block-order")
+    db_path_input_contact = os.path.join(db_path, "2_processed", "kinect")
+    db_path_input_led = os.path.join(db_path, "2_processed", "kinect")
+    # set output base directory
+    db_path_output = os.path.join(db_path, "2_processed", "kinect")
     if not os.path.exists(db_path_output):
         os.makedirs(db_path_output)
         print(f"Directory '{db_path_output}' created.")
@@ -70,32 +101,26 @@ if __name__ == "__main__":
         curr_contact_path_dir = os.path.join(db_path_input_contact, session)
         curr_contact_led_dir = os.path.join(db_path_input_led, session)
 
-        files_contact_abs, files_contact = path_tools.find_files_in_directory(curr_contact_path_dir, ending='_contact.csv')
+        files_contact_abs, files_contact = path_tools.find_files_in_directory(curr_contact_path_dir, ending=input_contact_filename_ending)
 
         for file_contact_abs, file_contact in zip(files_contact_abs, files_contact):
             print(f"current file: {file_contact}")
             # check if led and nerve files exist for this contact file
-            file_led = file_contact.replace("contact.csv", "LED.csv")
-            file_led_abs = os.path.join(curr_contact_led_dir, file_led)
-            try:
-                with open(file_led_abs, 'r'):
-                    pass
-            except FileNotFoundError:
-                print("Matching LED file does not exist.")
+            input_dirname = os.path.dirname(file_contact_abs)
+            files_led_abs, files_led = path_tools.find_files_in_directory(input_dirname, ending=input_led_filename_ending)
+            if not (isinstance(files_led_abs, list) and 1 == len(files_led_abs)):
+                print(f"Not exactly one {input_led_filename_ending} file detected.")
+                print(f"skip this file...")
                 continue
+            file_led_abs = files_led_abs[0]
+            file_led = files_led[0]
 
-            output_filename = file_contact.replace("_contact.csv", "_kinect.csv")
-            output_dir_abs = os.path.join(db_path_output, session)
-            if not os.path.exists(output_dir_abs):
-                os.makedirs(output_dir_abs)
+            output_dir_abs = input_dirname.replace(db_path_input_contact, db_path_output)
+            output_filename = create_output_filename(file_contact, file_led)
             output_filename_abs = os.path.join(output_dir_abs, output_filename)
-            if not force_processing:
-                try:
-                    with open(output_filename_abs, 'r'):
-                        print("Result file exists, jump to the next dataset.")
-                        continue
-                except FileNotFoundError:
-                    pass
+            if not force_processing and os.path.exists(output_filename_abs):
+                print(f"File '{file_contact}' already exists and the processing is not forced. Move to next file...")
+                continue
 
             contact = pd.read_csv(file_contact_abs)
             led = pd.read_csv(file_led_abs)
@@ -107,7 +132,7 @@ if __name__ == "__main__":
             # if the ratio of frames is too large, ignore the files
             if ratio > .05:
                 warnings.warn(f"number of elements are too different between contact and led csv files: {ratio*100}%")
-                contact_tmax = contact['t'].iloc[-1]
+                contact_tmax = contact['time'].iloc[-1]
                 led_tmax = led['time (second)'].iloc[-1]
                 diff_ms = (contact_tmax-led_tmax) * 1000
                 print(f"current filename = {file_contact.replace('_contact.csv', '')}:")
@@ -118,14 +143,15 @@ if __name__ == "__main__":
                 print(f"------------------\n")
 
             # prepare dataframes for merging
-            led = led.rename(columns={"time (second)": "t"}).round(3)
-            contact["t"] = contact["t"].round(3)
+            led = led.drop('green level', axis=1)
+            led = led.rename(columns={"time (second)": "time"}).round(3)
+            contact["time"] = contact["time"].round(3)
 
             # generate output dataframe
             df_output = contact
-            result = pd.merge(led, contact, on='t', how='outer')
-            df_output = pd.merge(df_output, led,  on='t', how='outer')
-
+            result = pd.merge(led, contact, on='time', how='outer')
+            df_output = pd.merge(df_output, led,  on='time', how='outer')
+            
             # keep variables for the report
             if generate_report:
                 diff_ms_all.append(len(contact)-len(led))
@@ -149,7 +175,7 @@ if __name__ == "__main__":
             writer.writeheader()
             for row in report_data:
                 writer.writerow(row)
-
+    
     print("done.")
 
 
