a57e2214934b3236d6b81853d8b90018d682992e
Basil <basil.duvernoy@gmail.com>
2025-08-29 11:00:10 +0200

    Remove work in progress tasks as they have been moved in their respective dev branches

--- code/scripts/_2_primary_processing/_2_generate_rgb_depth_video/__init__.py
+++ code/scripts/_2_primary_processing/_2_generate_rgb_depth_video/__init__.py
@@ -4,7 +4,3 @@
 from .generate_mkv_stream_analysis import generate_mkv_stream_analysis
 from .extract_depth_to_tiff import extract_depth_to_tiff
 from .extract_color_to_mp4 import extract_color_to_mp4
-from .create_no_background_color_videos import (
-    generate_foreground_masks,
-    remove_video_background
-)
