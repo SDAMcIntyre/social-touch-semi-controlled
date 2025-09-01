ebfe2e4bbabd78cf2c7ff7156e34add925360e4e
Basil <basil.duvernoy@gmail.com>
2025-09-01 19:46:27 +0200

    new content of the common subpackage of preprocessing to cleaner import

---
+++ code/src/preprocessing/common/__init__.py
@@ -0,0 +1,14 @@
+from .data_access.pc_data_handler import PointCloudDataHandler
+from .data_access.video_mp4_manager import VideoMP4Manager
+from .gui.video_frame_selector import VideoFrameSelector
+from .gui.frame_roi_square import FrameROISquare
+
+
+__all__ = [
+    'PointCloudDataHandler',
+    'VideoMP4Manager',
+    'VideoFrameSelector',
+    'FrameROISquare',
+]
+
+
