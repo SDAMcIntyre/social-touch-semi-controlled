94fa59e3cb132e34d4c6046b22494b68a8e62ccb
Basil <basil.duvernoy@gmail.com>
2025-11-26 11:13:56 +0100

    Update __init__.py

--- code/scripts/_3_preprocessing/_6_metadata_matching/__init__.py
+++ code/scripts/_3_preprocessing/_6_metadata_matching/__init__.py
@@ -2,3 +2,4 @@ from .define_trial_chunks import define_trial_chunks
 from .calculate_trial_id import calculate_trial_id
 from .generate_stimuli_metadata import generate_stimuli_metadata_to_data
 from .find_single_touches import find_single_touches
+from .review_single_touches import review_single_touches
