cb627a52a0d62fc2758b3a8ed2858de935a19481
Basil <basil.duvernoy@gmail.com>
2025-11-28 12:09:57 +0100

    Update compute_somatosensory_characteristics.py

--- code/scripts/_3_preprocessing/_4_somatosensory_quantification/compute_somatosensory_characteristics.py
+++ code/scripts/_3_preprocessing/_4_somatosensory_quantification/compute_somatosensory_characteristics.py
@@ -32,7 +32,7 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 def compute_somatosensory_characteristics(
         hand_motion_glb_path: Path, 
         hand_metadata_path: Path, 
-        metadata_path: Path,
+        forearm_metadata_path: Path,
         forearm_pointcloud_dir: Path,
         current_video_filename: str,
         output_csv_path: Path,
@@ -43,7 +43,7 @@ def compute_somatosensory_characteristics(
 ) -> str:
     if not should_process_task(
         output_paths=output_csv_path, 
-        input_paths=[hand_motion_glb_path, metadata_path], 
+        input_paths=[hand_motion_glb_path, forearm_metadata_path], 
         force=force_processing):
         print(f"✅ Output file '{output_csv_path}' already exists. Use force_processing to overwrite.")
         return
@@ -55,9 +55,9 @@ def compute_somatosensory_characteristics(
         print(f"Successfully loaded hand motion dictionary.")
 
     hand_metadata: HandMetadataManager = HandMetadataFileHandler.load(hand_metadata_path)
-
+    
     # Collect the forearms pointclouds for the specific video
-    forearm_params: List[ForearmParameters] = ForearmFrameParametersFileHandler.load(metadata_path)
+    forearm_params: List[ForearmParameters] = ForearmFrameParametersFileHandler.load(forearm_metadata_path)
     catalog = ForearmCatalog(forearm_params, forearm_pointcloud_dir)
     forearms_dict = get_forearms_with_fallback(catalog, current_video_filename)
 
